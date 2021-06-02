2f96a9958d22c680bcdeb0ef3c0c743919537bb1
dave8697 <gupta.sanket007@gmail.com>
2021-06-02 22:45:07 +0530

    Updated api

---
+++ Static/base.js
@@ -0,0 +1,74 @@
+async function search_books() {
+    const x = document.getElementById("search").value;
+    document.getElementById("cardsection").innerHTML = "";
+    document.getElementById("trash").innerHTML = "";
+    var spinner = document.getElementById("spinner");
+    spinner.classList.remove("visually-hidden");
+    await fetch_books(x);
+    spinner.classList.add("visually-hidden");
+}
+
+async function fetch_books(x) {
+    const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${x}`);
+    const result = await res.json();
+    console.log(result);
+    if (res.status == 200) {
+        var cardSection = document.getElementById("cardsection");
+        for (var i = 0; i < result.items.length; i++) {
+            var row;
+            var id;
+            var item = result.items[i];
+            if (i % 5 === 0) {
+                row = document.createElement("div");
+                row.classList.add("row");
+                id = `book-${i}`
+                row.setAttribute("id", id);
+            }
+            cardSection.appendChild(row);
+            var col = addelement(item.volumeInfo.imageLinks.thumbnail, item.volumeInfo.title, item.volumeInfo.publishedDate, item.volumeInfo.authors, item.volumeInfo.infoLink);
+            document.getElementById(id).appendChild(col);
+
+        }
+    }
+}
+function addelement(imgUrl, cardTitleText, publishedDate, authorText, extUrl) {
+    var column = document.createElement("div");
+    var cardParent = document.createElement("div");
+    var cardImg = document.createElement("img");
+    var cardBody = document.createElement("div");
+    var cardTitle = document.createElement("h5");
+    var author = document.createElement("p");
+    var cardText = document.createElement("p");
+    var moreBtn = document.createElement("a");
+
+    column.classList.add("col-6", "col-md", "d-flex", "align-items-stretch");
+    author.classList.add("author");
+    cardParent.classList.add("card");
+    cardImg.classList.add("card-img-top");
+    cardBody.classList.add("card-body");
+    cardTitle.classList.add("card-title");
+    cardText.classList.add("card-text");
+    moreBtn.classList.add("btn", "btn-primary");
+
+    cardImg.setAttribute("src", `${imgUrl}`);
+    cardImg.setAttribute("alt", `${cardTitleText}`);
+    moreBtn.setAttribute("href", `${extUrl}`);
+    moreBtn.setAttribute("target", "_blank");
+
+    author.innerHTML = `${authorText}`;
+    cardTitle.innerHTML = `${cardTitleText}`;
+    cardText.innerHTML = `Published on: ${publishedDate}`;
+    moreBtn.innerHTML = "Read More";
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(author);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(moreBtn);
+
+    cardParent.appendChild(cardImg);
+    cardParent.appendChild(cardBody);
+
+    column.appendChild(cardParent);
+
+    return column;
+}
